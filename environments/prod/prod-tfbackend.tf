f5107e0c2696ec505ec0a63627ba8f1573b75f42
Muthukumar Ramu <rl.muthukumar@gmail.com>
2026-03-28 01:20:14 +0530

    Implement professional multi-environment backend with partial configuration

--- environments/prod/prod-tfbackend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "tss-ebstalk-prod-backend"
-    key     = "prod/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
--- environments/qa/qa-tfbackend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "tss-ebstalk-qa-backend"
-    key     = "qa/terraform.tfstate"
-    region  = "us-east-1"
-    encrypt = true
-  }
-}
