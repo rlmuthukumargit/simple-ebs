cb1f40d6291cd97ae3da355534316974f6dab41f
Muthukumar Ramu <rl.muthukumar@gmail.com>
2026-03-30 00:06:28 +0530

    Add ADO pipeline and Terraform outputs for Spring Boot deployment

--- modules/elastic-beanstalk/outputs.tf
+++ modules/elastic-beanstalk/outputs.tf
@@ -2,3 +2,13 @@ output "eb_env_url" {
   description = "Elastic Beanstalk Environment URL"
   value       = aws_elastic_beanstalk_environment.eb_env.cname
 }
+
+output "eb_app_name" {
+  description = "Elastic Beanstalk Application Name"
+  value       = aws_elastic_beanstalk_application.eb_app.name
+}
+
+output "eb_env_name" {
+  description = "Elastic Beanstalk Environment Name"
+  value       = aws_elastic_beanstalk_environment.eb_env.name
+}
--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,13 @@ output "eb_env_url" {
   description = "Elastic Beanstalk Environment URL"
   value       = module.elastic_beanstalk.eb_env_url
 }
+
+output "eb_app_name" {
+  description = "Elastic Beanstalk Application Name"
+  value       = module.elastic_beanstalk.eb_app_name
+}
+
+output "eb_env_name" {
+  description = "Elastic Beanstalk Environment Name"
+  value       = module.elastic_beanstalk.eb_env_name
+}
